f5c622e14ac58fd33571a288cea6512dd3e920c8
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2024-01-28 13:18:04 +0100

    feat: install and configure config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get<number>('APPLICATION_PORT', 3000);
+
+  await app.listen(port);
 }
 bootstrap();
