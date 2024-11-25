65784cd6117312d8581430d30ac886629695a017
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2024-11-25 21:23:42 +0100

    fix: use smaller font by default

--- client/src/components/date-range-picker.jsx
+++ client/src/components/date-range-picker.jsx
@@ -10,9 +10,9 @@ export const DateRangePicker = ({ onStartDateChange }) => {
 
   return (
     <div className="inline-flex items-center justify-center gap-2 rounded-md bg-neutral-800 px-2 py-1">
-      <span className="text-sm font-bold uppercase">Date Range</span>
+      <span className="text-xs font-bold uppercase sm:text-sm">Date Range</span>
       <input type="date" max={start} className="rounded-md bg-neutral-900 px-2 py-1" onChange={handleStartDateChange} />
-      <span className="text-sm font-bold uppercase">to</span>
+      <span className="text-xs font-bold uppercase sm:text-sm">to</span>
       <input type="date" value={end} className="rounded-md bg-neutral-900 px-2 py-1" disabled />
     </div>
   );
