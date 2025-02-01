8f304da628cb3c43ac485851260048c8205b7e6e
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2025-02-01 23:55:21 +0100

    feat: add default value for start date

--- client/src/components/date-range-picker.jsx
+++ client/src/components/date-range-picker.jsx
@@ -1,8 +1,8 @@
 import { DateTime } from 'luxon';
 
-export const DateRangePicker = ({ onStartDateChange }) => {
-  const start = DateTime.now().minus({ days: 1 }).toFormat('yyyy-MM-dd');
-  const end = DateTime.now().toFormat('yyyy-MM-dd');
+export const DateRangePicker = ({ defaultStartDate, onStartDateChange }) => {
+  const maxStartDate = DateTime.now().minus({ days: 1 }).toFormat('yyyy-MM-dd');
+  const maxEndDate = DateTime.now().toFormat('yyyy-MM-dd');
 
   const handleStartDateChange = (event) => {
     onStartDateChange(event.target.value);
@@ -11,9 +11,21 @@ export const DateRangePicker = ({ onStartDateChange }) => {
   return (
     <div className="inline-flex items-center justify-center gap-2 rounded-md bg-neutral-800 px-2 py-1">
       <span className="text-xs font-bold uppercase sm:text-sm">Date Range</span>
-      <input type="date" max={start} className="rounded-md bg-neutral-900 px-2 py-1" onChange={handleStartDateChange} />
+      <input
+        type="date"
+        defaultValue={defaultStartDate}
+        max={maxStartDate}
+        className="rounded-md bg-neutral-900 px-2 py-1"
+        onChange={handleStartDateChange}
+      />
       <span className="text-xs font-bold uppercase sm:text-sm">to</span>
-      <input type="date" value={end} className="rounded-md bg-neutral-900 px-2 py-1" disabled />
+      <input
+        type="date"
+        defaultValue={maxEndDate}
+        max={maxEndDate}
+        className="rounded-md bg-neutral-900 px-2 py-1"
+        disabled
+      />
     </div>
   );
 };
--- client/src/components/filter-section.jsx
+++ client/src/components/filter-section.jsx
@@ -1,18 +1,18 @@
 import { DateRangePicker } from '@/components/date-range-picker';
-import { useFilterContext } from '@/contexts/filter-context';
+import { defaultContextValue, useFilterContext } from '@/contexts/filter-context';
 
 export const FilterSection = () => {
   const { setFilters } = useFilterContext();
 
   const handleStartDateChange = (since) => {
-    setFilters((filters) => {
-      return Object.assign({}, filters, { since: since });
+    setFilters((previous) => {
+      return Object.assign({}, previous, { since: since });
     });
   };
 
   return (
     <section className="mb-4 flex justify-end">
-      <DateRangePicker onStartDateChange={handleStartDateChange} />
+      <DateRangePicker defaultStartDate={defaultContextValue.filters.since} onStartDateChange={handleStartDateChange} />
     </section>
   );
 };
