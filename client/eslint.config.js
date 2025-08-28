a087ddd56af7ae846a2608cc961fb4aa0e52f60d
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2025-08-28 20:15:40 +0200

    chore: update vite dependencies

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -10,14 +10,27 @@ import eslint_simple_sort_plugin from 'eslint-plugin-simple-import-sort';
 
 export default [
   {
-    ignores: ['dist', 'node_modules'],
+    ignores: [
+      'dist',
+      'node_modules',
+      'eslint.config.js',
+      'postcss.config.js',
+      'prettier.config.js',
+      'tailwind.config.js',
+      'vite.config.js',
+    ],
   },
+  js.configs.recommended,
+  eslint_react_plugin.configs.flat.recommended,
+  eslint_react_plugin.configs.flat['jsx-runtime'],
+  eslint_react_hooks_plugin.configs['recommended-latest'],
+  eslint_react_refresh_plugin.configs.vite,
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
@@ -29,25 +42,13 @@ export default [
     },
     settings: {
       react: {
-        version: '18.3',
+        version: 'detect',
       },
     },
     plugins: {
-      'react': eslint_react_plugin,
-      'react-hooks': eslint_react_hooks_plugin,
-      'react-refresh': eslint_react_refresh_plugin,
       'simple-import-sort': eslint_simple_sort_plugin,
     },
     rules: {
-      // recommended rules
-      ...js.configs.recommended.rules,
-      ...eslint_react_plugin.configs.recommended.rules,
-      ...eslint_react_plugin.configs['jsx-runtime'].rules,
-      ...eslint_react_hooks_plugin.configs.recommended.rules,
-
-      // rules
-      'react/prop-types': 'off',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'object-shorthand': ['error', 'never'],
       'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
       'simple-import-sort/imports': [
@@ -56,6 +57,12 @@ export default [
           groups: [['^node:'], ['^react$', '^react-dom/client$'], ['^@?\\w'], ['^'], ['^\\.'], ['^\\u0000']],
         },
       ],
+    },
+  },
+  {
+    rules: {
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 ];
