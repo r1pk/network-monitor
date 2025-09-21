4a9ff2baaab388409d5c4ccc18e9081937fa2ca3
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2025-09-21 15:09:38 +0200

    refactor: migrate core functions to typescript

---
+++ client_ts/src/hooks/use-average-snapshot.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon';
+import useSWR, { type SWRResponse } from 'swr';
+
+import type { AverageSnapshot } from '@/types/average-snapshot';
+import { fetcher } from '@/utilities/fetcher';
+
+export const useAverageSnapshot = (since?: string): SWRResponse<AverageSnapshot, Error> => {
+  const url = import.meta.env.VITE_API_URL + '/api/snapshot/average';
+  const params = new URLSearchParams();
+
+  if (since) {
+    const date = DateTime.fromFormat(since, 'yyyy-MM-dd');
+
+    if (!date.isValid) {
+      throw new Error('Invalid date format: expected format yyyy-MM-dd');
+    }
+
+    params.append('since', date.toISO());
+  }
+
+  return useSWR<AverageSnapshot, Error>(url + '?' + params.toString(), fetcher);
+};
---
+++ client_ts/src/hooks/use-snapshot-list.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon';
+import useSWR, { type SWRResponse } from 'swr';
+
+import type { Snapshot } from '@/types/snapshot';
+import { fetcher } from '@/utilities/fetcher';
+
+export const useSnapshotList = (since?: string): SWRResponse<Snapshot[], Error> => {
+  const url = import.meta.env.VITE_API_URL + '/api/snapshot';
+  const params = new URLSearchParams();
+
+  if (since) {
+    const date = DateTime.fromFormat(since, 'yyyy-MM-dd');
+
+    if (!date.isValid) {
+      throw new Error('Invalid date format: expected format yyyy-MM-dd');
+    }
+
+    params.append('since', date.toISO());
+  }
+
+  return useSWR<Snapshot[], Error>(url + '?' + params.toString(), fetcher);
+};
