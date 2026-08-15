3d25e678eca495406bb6290131098b6e2373c588
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-08-15 02:57:47 +0200

    refactor: create separate database module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { DatabaseModule } from './database/database.module';
 import { SpeedTestModule } from './speedtest/speedtest.module';
 
 @Module({
@@ -11,26 +11,7 @@ import { SpeedTestModule } from './speedtest/speedtest.module';
       isGlobal: true,
       envFilePath: ['.env'],
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-
-        extra: {
-          decimalNumbers: true,
-        },
-
-        host: config.get('DATABASE_HOST') as string,
-        port: config.get('DATABASE_PORT') as number,
-        database: config.get('DATABASE_NAME') as string,
-        username: config.get('DATABASE_USER') as string,
-        password: config.get('DATABASE_PASSWORD') as string,
-
-        autoLoadEntities: true,
-        synchronize: config.get('DATABASE_SYNC_ENABLED') === 'true',
-      }),
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
     ScheduleModule.forRoot(),
     SpeedTestModule,
   ],
---
+++ server/src/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import databaseConfig from './database.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [databaseConfig.KEY],
+      useFactory: (config: ConfigType<typeof databaseConfig>) => ({
+        type: 'mysql',
+
+        extra: {
+          decimalNumbers: true,
+        },
+
+        host: config.host,
+        port: config.port,
+        database: config.name,
+        username: config.username,
+        password: config.password,
+
+        autoLoadEntities: true,
+        synchronize: config.synchronize,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
