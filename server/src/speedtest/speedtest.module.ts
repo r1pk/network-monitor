bffceaa5acdf3f6906e56221b2bb685afbfb2607
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-08-16 13:50:57 +0200

    refactor change import name

--- server/src/speedtest/speedtest.module.ts
+++ server/src/speedtest/speedtest.module.ts
@@ -4,10 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { SnapshotModule } from '../snapshot/snapshot.module';
 import { SpeedtestScheduler } from './scheduler/speedtest.scheduler';
 import { SpeedtestService } from './service/speedtest.service';
-import configuration from './speedtest.config';
+import config from './speedtest.config';
 
 @Module({
-  imports: [ConfigModule.forFeature(configuration), SnapshotModule],
+  imports: [ConfigModule.forFeature(config), SnapshotModule],
   providers: [SpeedtestScheduler, SpeedtestService],
 })
 export class SpeedtestModule {}
