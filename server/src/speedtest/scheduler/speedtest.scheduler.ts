fe82afd824b9c029fcbe19fb3fcde41b8b710e59
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-08-16 14:23:43 +0200

    refactor: remove unnecessary variable

--- server/src/speedtest/scheduler/speedtest.scheduler.ts
+++ server/src/speedtest/scheduler/speedtest.scheduler.ts
@@ -2,7 +2,6 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 import { SnapshotService } from '../../snapshot/service/snapshot.service';
-import type { SnapshotData } from '../../snapshot/type/snapshot-data.type';
 import { SpeedtestService } from '../service/speedtest.service';
 
 @Injectable()
@@ -18,25 +17,28 @@ export class SpeedtestScheduler {
   async run(): Promise<void> {
     try {
       const result = await this.speedtestService.run();
-      const data: SnapshotData = {
-        download: null,
-        upload: null,
-        ping: null,
-        loss: null,
-        host: null,
-        url: null,
-      };
 
-      if (result) {
-        data.download = result.download.bandwidth;
-        data.upload = result.upload.bandwidth;
-        data.ping = result.ping.latency;
-        data.loss = result.packetLoss;
-        data.host = result.server.host;
-        data.url = result.result.url;
+      if (result === null) {
+        await this.snapshotService.create({
+          download: null,
+          upload: null,
+          ping: null,
+          loss: null,
+          host: null,
+          url: null,
+        });
+
+        return;
       }
 
-      await this.snapshotService.create(data);
+      await this.snapshotService.create({
+        download: result.download.bandwidth,
+        upload: result.upload.bandwidth,
+        ping: result.ping.latency,
+        loss: result.packetLoss,
+        host: result.server.host,
+        url: result.result.url,
+      });
     } catch (error) {
       this.logger.error('Failed to execute scheduled speedtest', error);
     }
