1e4bf224b94988848b6659452b23a85799b65125
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-09-17 19:31:21 +0200

    chore: use sqlite as database engine

--- server/src/database/database.config.ts
+++ server/src/database/database.config.ts
@@ -1,17 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  type: 'mysql' as const,
+  type: 'better-sqlite3' as const,
 
-  extra: {
-    decimalNumbers: true,
-  },
-
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT || 3306),
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_PATH,
 
   autoLoadEntities: true,
   synchronize: ['true', '1'].includes(process.env.DATABASE_SYNCHRONIZE?.toLowerCase() || 'false'),
