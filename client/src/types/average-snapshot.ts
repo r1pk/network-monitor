e2aebedd23b7bc9acec003e7b53db251125d69ca
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2026-09-18 23:37:20 +0200

    chore: remove docker setup and client module

--- client/src/types/average-snapshot.ts
+++
@@ -1,6 +0,0 @@
-export type AverageSnapshot = {
-  download: number;
-  upload: number;
-  ping: number;
-  loss: number;
-};
--- client/src/utilities/convert-bytes-to-megabits.ts
+++
@@ -1,3 +0,0 @@
-export const convertBytesToMegabits = (bytes: number): number => {
-  return Math.round(bytes / 125000);
-};
